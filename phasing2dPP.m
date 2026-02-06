5b7d89461566784c26cbe15e5fc67b44bcdb1c06
Ryan Wheat <45976754+rmw83@users.noreply.github.com>
2026-02-06 16:00:46 -0500

    Update phasing2dPP.m
    An adjustment was needed to account for unique cases where a gap is present in `findvarbool`.

--- phasing2dPP.m
+++ phasing2dPP.m
@@ -83,8 +83,8 @@ global c_cmfs wavenumbersToInvFs fringeToFs
 var = movvar(abs_array, floor(sqrt(length(abs_array))));
 varbool = var >= 0.5*max(var);
 findvarbool = find(varbool);
-fit_points_offset = floor(length(findvarbool)/2);
-i_max = findvarbool(fit_points_offset);
+fit_points_offset = floor(findvarbool(end) - findvarbool(1))/2;
+i_max = findvarbool(1) + fit_points_offset;
 
 %rough guess
 %tau = 1/w(i_max)/wavenumbersToInvFs;
